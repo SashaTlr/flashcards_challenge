6aad9968a95fa9e85a99fd631c5ba9d6c64adb9a
Sasha & Luis Fernando Plaz <pair+SashaTlr.luisplaz@devbootcamp.com>
2015-12-11 16:36:38 -0500

    Add Spec Files

--- source/checker.rb
+++ source/checker.rb
@@ -2,7 +2,7 @@ module Checkable
 
   require 'pry'
 
-  def check_match(element_1,element_2)
+  def self.check_match(element_1,element_2)
 
     element_1 == element_2
 
--- source/parser.rb
+++ source/parser.rb
@@ -13,11 +13,8 @@ module Parseable
 
     text_line_array.each_slice(2) {|item| def_answer_array << [item[0],item[1]]}
 
-    binding.pry
-
     return def_answer_array
 
   end
 end
 
-p Parseable.parse_file('flashcard_samples.txt')
