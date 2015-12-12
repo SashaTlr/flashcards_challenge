a8dd35530ef25ff4cc31fc60d809918814b75656
Sasha & Luis Fernando Plaz <pair+SashaTlr.luisplaz@devbootcamp.com>
2015-12-12 14:46:42 -0500

    Add Chnages to Controller

--- source/deck.rb
+++ source/deck.rb
@@ -1,18 +1,20 @@
-require_relative 'parser' # returns [['q','a'],['q2','a2'],...]
+require_relative 'parser'
 require_relative 'flashcards'
 require 'pry'
 
 class Deck
-
   include Parseable
   attr_reader :deck
 
-  def initialize(file) #array
+  def initialize(file)
     @deck = parse_file(file)
   end
 
   def next_flashcard
-    @deck.pop
+    @deck.shuffle!.shift
   end
 
+  def add_card_to_deck(card)
+    @deck.push(card) if !@deck.include?(card)
+  end
 end
